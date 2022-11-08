5b15f9c14e610d99dcafeb94d735f490b363f7b8
eliszewski <liszewski31098@gmail.com>
2022-11-08 13:26:26 -0500

    start findColumn

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -39,9 +39,10 @@ public class Board {
     public Boolean findColumn(String x){
         for(int i = 0; i <=3; i++){
             for(int j = 0; j <=3; j++){
-                board[i][j] =
+
             }
         }
+        return null;
     }
 }
 
