657682d9f47e4a30784a6477eebfd9154644c523
Eryk <eryk@Zipcoders-MacBook-Pro-71.local>
2022-11-09 16:22:44 -0500

    checkpoint

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -6,8 +6,11 @@ import com.sun.org.apache.xpath.internal.operations.Bool;
  * @author leon on 6/22/18.
  */
 public class Board {
-    Character[][] board;
-
+    public Character[][] board;
+    public String winner;
+    public boolean xWin;
+    public boolean oWin;
+    public boolean Tie;
 
     public Board(Character[][] matrix) {
         this.board = matrix;
@@ -29,17 +32,21 @@ public class Board {
     public String getWinner() {
         return null;
     }
-    public  Boolean findDiag(String xOrO){
+    public  Boolean findDiag(Character xOrO){
 
         if((xOrO.equals(board[0][0]) && xOrO.equals(board[1][1]) && xOrO.equals(board[2][2])) || (xOrO.equals(board[2][0]) && xOrO.equals(board[1][1]) && xOrO.equals(board[0][2]))){
             return true;
         }
         return false;
     }
-    public Boolean findColumn(String x){
-        for(int i = 0; i <=3; i++){
-            for(int j = 0; j <=3; j++){
+    public Boolean findColumn(Character xo){
+        int matchDet = 0;
+        int c = 0;
+        for(int i = 0; i <= board[0].length; i++){
+            for(int j = i;j <=board[0].length; j++){
+                if(board[j][i].equals(xo){
 
+                }
             }
         }
         return null;
